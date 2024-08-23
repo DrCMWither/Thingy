ac247743cad528503a23bcb46c6f101cd7b61fa2
DrCMWither <69045396+DrCMWither@users.noreply.github.com>
2024-08-23 09:10:44 +0800

    Add files via upload

---
+++ leetcode-exercises/Sudoku Solver/cpp/Sudoku-Solver.cpp
@@ -0,0 +1,17 @@
+/*
+    Status: Compile Error
+    Time:   N/A  ms
+    Mem:    N/A  MB
+    TimC:   N/A
+    MemC:   N/A
+*/
+
+#include <vector>
+
+class Solution {
+public:
+    void solveSudoku(std::vector<std::vector<char>>& board) {
+        board = 你靠感觉吧(board);
+        return board;
+    }
+};
