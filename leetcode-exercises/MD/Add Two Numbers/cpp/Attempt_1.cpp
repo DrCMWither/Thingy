99cb69f75c17edcb5b3585acc76c35dece8a9f7e
DrCMWither <69045396+DrCMWither@users.noreply.github.com>
2024-08-23 10:11:11 +0800

    Add files via upload

---
+++ leetcode-exercises/MD/Add Two Numbers/cpp/Attempt_1.cpp
@@ -0,0 +1,42 @@
+/*
+    Status: Accepted
+    Time:   29   ms
+    Mem:    77.4 MB
+    TimC:   O(n)
+    MemC:   O(n)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x),   
+ next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* current = head;
+        
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum  = (l1 ? l1 -> val : 0) + (l2 ? l2 -> val : 0) + carry;
+            carry = sum / 10;
+            current -> next = new ListNode(sum % 10);
+            current = current -> next;
+
+            if (l1) l1 = l1 -> next;
+            if (l2) l2 = l2 -> next;
+        }
+
+        return head -> next; 
+
+    }
+};
---
+++ leetcode-exercises/MD/Add Two Numbers/cpp/Attempt_2.cpp
@@ -0,0 +1,48 @@
+/*
+    Status: Accepted
+    Time:   23   ms
+    Mem:    77.4 MB
+    TimC:   O(n) maybe
+    MemC:   O(n) maybe
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x),   
+ next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* current = head;
+
+        int carry = 0;
+
+        if (!l1 && !l2) {
+            return head -> next;
+        }
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2 -> val : 0) + carry;
+            carry = sum / 10;
+            ListNode* newNode = new ListNode(sum % 10);
+            current -> next = newNode;
+            current = newNode;
+
+            l1 = l1 ? l1 -> next : nullptr;
+            l2 = l2 ? l2 -> next : nullptr;
+        }
+
+        return head->next;
+
+    }
+};
+
+//I'm writing shit, I need to sleep.
